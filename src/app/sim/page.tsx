03714e069e5fec712619f5f06a7a7d380b160007
Joey Hills <joeymhills@gmail.com>
2024-09-23 23:36:56 -0400

    implemented being able to use your builds in sim

--- src/app/sim/page.tsx
+++ src/app/sim/page.tsx
@@ -4,22 +4,37 @@ import Link from "next/link";
 import { Hero } from "../lib/Hero";
 import { HeroAttributesMap } from "~/types/types";
 import heroData from "~/heroes.json";
-import { Sim } from "../lib/sim";
 import SimComponent from "~/components/Sim";
+import { getSavedBuilds, loadBuild } from "../lib/Builds";
+import { useState, useEffect } from "react";
 
 export default function SimPage() {
 
-  let hero1Name = "Abrams";
-  let hero2Name = "Bebop";
-  const heroes: HeroAttributesMap = heroData;
+    const heroes: HeroAttributesMap = heroData;
+    const buildNames: string[] = getSavedBuilds() as string[];
+    const [heroList, setHeroList] = useState<Hero[]>([]);
 
-  //TODO: Take another look at this
-  const heroA = heroes[hero1Name] ? new Hero(heroes[hero1Name]) : new Hero();
-  const heroB = heroes[hero2Name] ? new Hero(heroes[hero2Name]) : new Hero();
+    const addBuild = (buildName: string) => {
+        setHeroList((heroList) => [...heroList, new Hero(loadBuild(buildName))])
+    }
 
     return (
-        <main className="flex min-h-screen flex-col items-center text-white">
-            <SimComponent heroAData={heroA} heroBData={heroB}/>
+        <main className="flex min-h-screen flex-col justify-center items-center text-white">
+            {heroList.length < 2 ? (
+                <div>
+                    {buildNames?.map(build => (
+                        <div onClick={() => addBuild(build)}>{build}</div>
+                    )
+                    )}
+                </div>
+            ) : (
+                <div>
+                    {heroList.length == 2 ? (
+                        <SimComponent heroAData={heroList[0] as Hero} heroBData={heroList[1] as Hero} />
+                    ) : <div></div>
+                    }
+                </ div>
+            )}
         </main>
     );
 }
